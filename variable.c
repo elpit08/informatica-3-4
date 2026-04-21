0497fbaa9ec5c86a71b362a8b8d40056331f6041
elpit08 <pgdr250920@gmail.com>
2026-04-21 21:38:02 +0000

    we made variables and corrected to make them work

---
+++ variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    int number; //I ereased the !
+int test_value; // I used _ instead of the space
+int n1example; // I started the variable with a letter
+float intburguer; // I didnt let int be the name of the variable so I putted burguer
+printf("Variables created successfully.\n");
+}
