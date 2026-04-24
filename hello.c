a30504d517acfd11d1f3160e793e88a88aeb779d
elpit08 <pgdr250920@gmail.com>
2026-04-24 14:49:13 +0000

    use scanf for the first time and learn how to use word

--- hello.c
+++ hello.c
@@ -2,5 +2,12 @@
 
 int main(void)
 {
-  printf("Hello Pedro! \n");
+  char user[20];
+  printf("Hello:  ");
+  scanf("%s", &user );
+  printf("Hello, %s \n", user);
+  char color[15];
+  printf("What is your favorite color: \n");
+  scanf("%s", &color);
+  printf("Your favorite color is %s \n", color);
 }
