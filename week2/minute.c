12cd2c2ab194aa00d9e8e05c4fcf82078e54dda5
elpit08 <pgdr250920@gmail.com>
2026-05-07 19:29:42 +0000

    no se pudo hacer el codigo

--- week2/minute.c
+++ week2/minute.c
@@ -5,12 +5,19 @@ int main(void){
     int Min;
     printf("What is your starting time \n");
     scanf("%d", &Hr);
+    printf( " \033[1A :");
     scanf("%d", &Min);
     printf(" How long is the movie \n");
     scanf("%d", &Mvt);
+    int Mvmin = Mvt%60;
     int FHr = Hr+Mvt/60;
     int Tdu = Mvt/60;
     int FMin =(Mvt+Min)%60;
-    printf("This movie will Last %d hours and %02d minutes \n", Tdu, FMin);
+if( FMin >= 60){
+    FHr = FHr + (FMin / 60);
+    FMin = FMin % 60;
+}
+
+    printf("This movie will Last %d hours and %02d minutes \n", Tdu, Mvmin);
     printf(" Your finish time is: %d:%02d \n", FHr, FMin);
 }
